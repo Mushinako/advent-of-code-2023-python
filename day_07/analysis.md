f8d401dac2164f704c29449e5d49617906dad182
Mushinako <github@mushinako.me>
2023-12-06 20:35:05 -0800

    Day 07 - Preparations

---
+++ day_07/analysis.md
@@ -0,0 +1,5 @@
+# Day 07 (XXX)
+
+## Part 1
+
+## Part 2
---
+++ day_07/solution.py
@@ -0,0 +1,22 @@
+# pyright: reportMissingTypeStubs=false
+
+from __future__ import annotations
+
+from utils import SolutionAbstract
+
+
+class Solution(SolutionAbstract, day=7):
+    def _process_data(self, raw_data: list[str]) -> None:
+        """
+        Process day 07 data.
+        """
+
+    def part_1(self) -> ...:
+        """
+        Day 07 part 1 solution.
+        """
+
+    def part_2(self) -> ...:
+        """
+        Day 07 part 2 solution.
+        """
