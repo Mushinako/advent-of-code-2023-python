c1d1a82f3f9abe3afb39e03ecf551fef6fe1bf7f
Mushinako <github@mushinako.me>
2023-12-11 19:23:02 -0800

    Day 13 - Preparations

---
+++ day_12/solution.py
@@ -0,0 +1,22 @@
+# pyright: reportMissingTypeStubs=false
+
+from __future__ import annotations
+
+from utils import SolutionAbstract
+
+
+class Solution(SolutionAbstract, day=12):
+    def _process_data(self, raw_data: list[str]) -> None:
+        """
+        Process day 12 data.
+        """
+
+    def part_1(self, *, visualize: bool = False) -> ...:
+        """
+        Day 12 part 1 solution.
+        """
+
+    def part_2(self, *, visualize: bool = False) -> ...:
+        """
+        Day 12 part 2 solution.
+        """
